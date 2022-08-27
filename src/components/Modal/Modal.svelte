8b75567846b274d8cee1b1d65926c09471b095f1
kushagra-aa <kushagraagnihotri24@gmial.com>
2022-08-27 12:41:35 +0530

    added options conponent

---
+++ src/components/Modal/Modal.svelte
@@ -0,0 +1,81 @@
+<script>
+  import CopyIcon from "./../../assets/Copy.svg";
+  import RefreshIcon from "./../../assets/Refresh.svg";
+  import Options from "./Options.svelte";
+  let password =
+    "afsdnbahsdjkflbghkfasdbghjkfvdbavlhjkbhdjklsgbfhjlkadsgfhjkgbsdakjhfbfkhjdsbflhjkdsbfdlhsjkvbhj";
+  let length = 10;
+  let upper = 0;
+  let lower = 0;
+  let numbers = 0;
+  let symbols = 0;
+  let isLength = false;
+  let isUpper = false;
+  let isLower = false;
+  let isNumbers = false;
+  let isSymbols = false;
+  let isWord = false;
+  let word = "";
+  $: if (length) {
+    console.log(length);
+  }
+  $: if (upper) {
+    console.log(upper);
+  }
+  $: if (lower) {
+    console.log(lower);
+  }
+  $: if (numbers) {
+    console.log(numbers);
+  }
+  $: if (symbols) {
+    console.log(symbols);
+  }
+  $: if (word) {
+    console.log(word);
+  }
+  const copyToClipBoard = () => {
+    navigator.clipboard.writeText(password);
+  };
+</script>
+
+<div class="modal-container">
+  <div class="title-container">
+    <h1>generate a random password</h1>
+    <h2>Use this Amzing tool create Fun, Secure and Random passwords</h2>
+  </div>
+  <div class="password-box">
+    <p>{password}</p>
+    <div class="pass-buttons">
+      <button on:click={copyToClipBoard}><CopyIcon /></button>
+      <button><RefreshIcon /></button>
+    </div>
+  </div>
+  <div class="presets-container">
+    <h3>presets:</h3>
+    <button>perfect</button>
+    <button>random</button>
+    <button>minimum</button>
+    <button class="genreate-btn">generate</button>
+  </div>
+  <div class="custom-container">
+    <h3>custom:</h3>
+    <Options title="length" value={length} isValue={isLength} />
+    <Options title="uppercase" value={upper} isValue={isUpper} />
+    <Options title="lowercase" value={lower} isValue={isLower} />
+    <Options title="numbers" value={numbers} isValue={isNumbers} />
+    <Options title="symbols" value={symbols} isValue={isSymbols} />
+    <Options
+      title="word"
+      value={word}
+      isValue={isWord}
+      isWords={true}
+      {length}
+      classNames={{
+        container: "words-option-container",
+        number: "",
+        range: "",
+      }}
+    />
+  </div>
+</div>
---
+++ src/components/Modal/Options.svelte
@@ -0,0 +1,52 @@
+<script>
+  export let title;
+  export let value;
+  export let isValue;
+  export let isWords = false;
+  export let length = 0;
+  export let classNames = {
+    container: "",
+    number: "",
+    range: "",
+  };
+</script>
+
+<div class={`${classNames.container} options-container`}>
+  <h4>{title}</h4>
+  <div class="tickbox-container">
+    <input type="checkbox" bind:checked={isValue} />
+    <span class="checkmark" />
+  </div>
+  {#if isWords}
+    <input
+      class={`${!isValue ? "disabled-input" : "enabled-input"}`}
+      disabled={!isValue}
+      type="text"
+      bind:value
+      maxlength={length}
+    />
+    <p>
+      {value.length}
+    </p>
+  {/if}
+  {#if !isWords}
+    <input
+      class={`${classNames.number} ${
+        !isValue ? "disabled-input" : "enabled-input"
+      }`}
+      disabled={!isValue}
+      type="number"
+      bind:value
+    />
+    <input
+      class={`${classNames.range} ${
+        !isValue ? "disabled-input" : "enabled-input"
+      }`}
+      disabled={!isValue}
+      type="range"
+      min="1"
+      max="60"
+      bind:value
+    />
+  {/if}
+</div>
