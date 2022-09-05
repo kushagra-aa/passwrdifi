3bd624dabbc8e82e51e0ffd11d04adb3d7c068e1
Kushaga Agnihotri <kushagraagnihotri24@gmail.com>
2022-09-05 16:37:44 +0530

    Update Api.js

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Create a instance of axios to use the same base url.
 const axiosAPI = axios.create({
-  baseURL: " http://localhost:8000", // it's not recommended to have this info here.
+  baseURL: "https://apisby-k.herokuapp.com", // it's not recommended to have this info here.
 });
 
 // implement a method to execute all the request from here.
