a438bca888d418df0250234495245f52c9bdc2dd
kushagra-aa <kushagraagnihotri24@gmial.com>
2023-06-27 18:37:44 +0530

    changed api url

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Create a instance of axios to use the same base url.
 const axiosAPI = axios.create({
-  baseURL: "https://apisby-k.herokuapp.com", // it's not recommended to have this info here.
+  baseURL: "https://kushagraaa7.pythonanywhere.com", // it's not recommended to have this info here.
 });
 
 // implement a method to execute all the request from here.
