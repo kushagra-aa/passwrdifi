8c78677923af3758a7e81b5d0c0235e6c756d141
kushagra-aa <kushagraagnihotri24@gmial.com>
2023-07-04 15:51:50 +0530

    added basic routing for home and /play

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,24 @@
 <script>
   import Modal from "./components/Modal/Modal.svelte";
+  import Home from "./page/Home.svelte";
+
+  let page =
+    window.location.href.split("://")[1].split("/")[1] === ""
+      ? "/"
+      : `/${window.location.href.split("://")[1].split("/")[1]}`;
+  console.log(page);
+  const changePage = () => {
+    page = "/play";
+  };
 </script>
 
 <main>
-  <Modal />
+  {#if page === "/"}
+    <Home {changePage} />
+  {/if}
+  {#if page === "/play"}
+    <Modal />
+  {/if}
 </main>
 
 <style>
