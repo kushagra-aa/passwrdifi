a99542427f2944314b8789877d48dc32c964f2d3
kushagra-aa <kushagraagnihotri24@gmial.com>
2023-07-04 13:28:51 +0530

    added tailwind css

---
+++ src/page/Home.svelte
@@ -0,0 +1,8 @@
+<script>
+  import "./../styles/home.css";
+
+  export let changePage = () => {};
+</script>
+
+<h1 class="text-red-500">Hello</h1>
+<a href="/play" on:click={changePage}> click </a>
